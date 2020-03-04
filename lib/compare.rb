0cb4b4a3b2ec32274b9188dab3572faceaf14f67
bren2102 <brb.yg.21@gmail.com>
2020-03-04 10:22:51 -0500

    Fix compare method

--- lib/compare.rb
+++ lib/compare.rb
@@ -1,5 +1,4 @@
 class Compare
-
   def evaluate_line(line)
     if line.count('<') == 2 && line.count("\/") == 1 && line.count('>') == 2
       true
@@ -8,24 +7,30 @@ class Compare
     end
   end
 
-  def compare_same_tag(line)
+  def get_tag_initial(line)
     if line.index(' ')
       if line.index(' ') < line.index('>')
-        tag_initial = line[(line.index('<')+1)...line.index(' ')]
+        tag_initial = line[(line.index('<') + 1)...line.index(' ')]
       elsif line.index(' ') > line.index('>')
-        tag_initial = line[(line.index('<')+1)...line.index('>')]
+        tag_initial = line[(line.index('<') + 1)...line.index('>')]
       end
     elsif !line.index(' ')
-      tag_initial = line[(line.index('<')+1)...line.index('>')]
+      tag_initial = line[(line.index('<') + 1)...line.index('>')]
     end
-    tag_end = line[(line.index("\/")+1)...line.rindex('>')]
-    if (tag_initial == tag_end)
+    tag_initial
+  end
+
+  def get_tag_end(line)
+    line[(line.index("\/") + 1)...line.rindex('>')]
+  end
+
+  def compare_same_tag(line)
+    tag_initial = get_tag_initial(line)
+    tag_end = get_tag_end(line)
+    if tag_initial == tag_end
       puts 'Tags Match'
     else
       puts '[ERROR] Tags do not Match'
     end
   end
 end
-
-#compare = Compare.new
-#puts compare.compare_same_tag('<Movies genre = “Science Fiction”>',0)
