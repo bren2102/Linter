a9acca173ed82dc26e6c4d1b85f7ae02f8146b97
bren2102 <brb.yg.21@gmail.com>
2020-03-04 13:16:03 -0500

    Add multilineal validation

--- bin/index.rb
+++ bin/index.rb
@@ -6,15 +6,24 @@ file = File.open("doc.xml")
 #file.foreach("doc.xml") { |line| puts line}
 
 File.foreach("doc.xml").with_index do |line, line_num|
-   puts "#{line_num}: #{line}"
-   if compare.evaluate_line(line) == true
-     compare.compare_same_tag(line)
-   end
-   if line_num == 0
-     compare.compare_first_line(line)
-   end
+  puts "#{line_num}: #{line}"
+  if compare.evaluate_line(line) == true
+    compare.compare_same_tag(line)
+  end
+
+  if line_num == 0
+    compare.compare_first_line(line)
+  end
+
+  if compare.evaluate_multilineal_tag(line) == 1 
+    compare.get_multilineal_initial_tag(line)
+  elsif compare.evaluate_multilineal_tag(line) == 2
+    compare.get_multilineal_lasting_tag(line)
+  end
+
 end
 
+compare.compare_multilineal
 #file_data = file.readlines.map(&:chomp)
 #puts file_data
 file.close
--- lib/compare.rb
+++ lib/compare.rb
@@ -1,4 +1,8 @@
 class Compare
+  def initialize()
+    @multilineal_tag = []
+  end
+
   def evaluate_line(line)
     if line.count('<') == 2 && line.count("\/") == 1 && line.count('>') == 2
       true
@@ -35,7 +39,7 @@ class Compare
   end
 
   def get_first_line(line)
-    return line if line[2]=='x' && line[4]=='l'
+    return line if line[2] == 'x' && line[4] == 'l'
   end
 
   def compare_first_line(line)
@@ -44,6 +48,34 @@ class Compare
       puts 'First line exists'
     else
       puts '[WARNING]'
+    end
+  end
+
+  def evaluate_multilineal_tag(line)
+    if line.count('<') == 1 && line.count('>') == 1 && line.count("\/") == 0 && line.count('?') == 0
+      return 1
+    elsif line.count('<') == 1 && line.count('>') == 1 && line.count("\/") == 1
+      return 2
+    else
+      return false
+    end
+  end
+
+  def get_multilineal_initial_tag(line)
+    puts line
+    @multilineal_tag.push(line[(line.index('<') + 1)...line.index(' ')]) 
+  end
+
+  def get_multilineal_lasting_tag(line)
+    @multilineal_tag.delete(line[(line.index("\/") + 1)...line.index('>')]) 
+  end
+
+  def compare_multilineal
+    puts @multilineal_tag
+    if !@multilineal_tag.empty?
+      puts "[ERROR] open tag on #{@multilineal_tag}" 
+    else
+      puts 'No open tag found'
     end
   end
 end
